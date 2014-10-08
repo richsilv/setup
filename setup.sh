1e6a3a71203e41f7178cb0cd292e5b5d83e39884
Richard Silverton <silverton.richard@googlemail.com>
2014-10-08 23:17:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/v0.17.2/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
